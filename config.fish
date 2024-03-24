b5264d312015da7fceb330122225efb48c82ca93
cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2024-03-24 21:56:51 +0000

    :sparkles: fish_add_path node bin

--- config.fish
+++ config.fish
@@ -11,13 +11,14 @@ if status is-interactive
     fish_add_path ~/.cargo/bin
     fish_add_path ~/.fly/bin
     fish_add_path ~/.bun/bin
+    fish_add_path /home/vampire/.nvm/versions/node/v21.4.0/bin
 
     set -x DPRINT_INSTALL "/home/vampire/.dprint"
     set -x PATH "$DPRINT_INSTALL/bin" $PATH
     set -gx PATH $PATH /usr/bin/node
     set -gx PATH $HOME/.local/share/lua-language-server/bin $PATH
     set -gx PATH $PATH /home/vampire/.local/share/bob/nvim-bin
-    set -gx PATH $PATH /home/vampire/.nvm/versions/node/v21.4.0/bin
+    # set -gx PATH $PATH /home/vampire/.nvm/versions/node/v21.4.0/bin
     set -x DENO_INSTALL /home/vampire/.deno
     set -x PATH $DENO_INSTALL/bin $PATH
 end
